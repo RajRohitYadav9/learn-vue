cbc155c79eb2f9869bc2a5a34faf3bc048a81886
RajRohitYadav9 <rjrohityadav19@gmail.com>
2024-07-19 16:55:42 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   src/components/Home.vue
    
    Changes not staged for commit:
    	modified:   src/components/Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,70 +1,92 @@
 <template>
-    <div>
-        <div v-if="loading" class="loading">Loading...</div>
-        <div v-else>
-            <div class="container">
-                <h1 class="heading">Posts of all users</h1>
-                <div>
-                  <input
-                    type="text" placeholder="Search by author or title"
-                    v-model="searchQuery"
-                  />
-                </div>
-            <ul class="post-list">
-                <li v-for="post in filteredPosts" :key="post.id" class="post-item">
-                <h2 class="post-title"><router-link :to="`/post/${post.id}`">{{post.id}}. {{ post.title }}</router-link></h2>
-                <p class="post-body">{{ post.body }}</p>
-                <p class="post-author"><strong>By:</strong> {{ post.userName }}</p>
-                </li>
-            </ul>
-            </div>
+  <div>
+    <div v-if="loading" class="loading">Loading...</div>
+    <div v-else>
+      <div class="container">
+        <h1 class="heading">Posts of all users</h1>
+        <div>
+          <input
+            type="text"
+            placeholder="Search by author or title"
+            v-model="searchQuery"
+          />
+          <button @click="toggleSort">
+            Sort by {{ sortBy === 'title' ? 'ID' : 'Title' }}
+          </button>
         </div>
+        <ul class="post-list">
+          <li v-for="post in filteredPosts" :key="post.id" class="post-item">
+            <h2 class="post-title">
+              <router-link :to="`/post/${post.id}`">{{post.id}}. {{ post.title }}</router-link>
+            </h2>
+            <p class="post-body">{{ post.body }}</p>
+            <p class="post-author"><strong>By:</strong> {{ post.userName }}</p>
+          </li>
+        </ul>
+      </div>
     </div>
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  
-  export default {
-    name: 'Home',
-    data() {
-      return {
-        posts: [],
-        loading: true,
-        searchQuery: ''
-      };
-    },
-    computed: {
-      filteredPosts(){
-        if (!this.searchQuery){
-          return this.posts
-        }
-        return this.posts.filter(post =>
-          (post.userName.toLowerCase().includes(this.searchQuery.toLowerCase()) || post.title.toLowerCase().includes(this.searchQuery.toLowerCase()))
-        )
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      posts: [],
+      loading: true,
+      searchQuery: '',
+      sortBy: 'title', // default sort by title
+    };
+  },
+  computed: {
+    filteredPosts() {
+      let sortedPosts = [...this.posts];
+      if (this.sortBy === 'title') {
+        sortedPosts.sort((a, b) => a.title.localeCompare(b.title));
+      } else if (this.sortBy === 'id') {
+        sortedPosts.sort((a, b) => a.id - b.id);
       }
-    },
-    async created() {
-      try {
-        const postResponse = await axios.get('https://jsonplaceholder.typicode.com/posts');
-        const posts = postResponse.data;
-        
-        const usersPromises = posts.map(post => axios.get(`https://jsonplaceholder.typicode.com/users/${post.userId}`));
-        const usersResponses = await Promise.all(usersPromises);
-        
-        const users = usersResponses.map(response => response.data);
-        
-        this.posts = posts.map(post => ({
-          ...post,
-          userName: users.find(user => user.id === post.userId).name
-        }));
-        this.loading = false;
-      } catch (error) {
-        console.error('Error fetching posts or users:', error);
+      if (!this.searchQuery) {
+        return sortedPosts;
       }
+      return sortedPosts.filter(
+        (post) =>
+          post.userName.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+          post.title.toLowerCase().includes(this.searchQuery.toLowerCase())
+      );
     },
-  };
-  </script>
+  },
+  methods: {
+    toggleSort() {
+      this.sortBy = this.sortBy === 'title' ? 'id' : 'title';
+    },
+  },
+  async created() {
+    try {
+      const postResponse = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      const posts = postResponse.data;
+
+      const usersPromises = posts.map((post) =>
+        axios.get(`https://jsonplaceholder.typicode.com/users/${post.userId}`)
+      );
+      const usersResponses = await Promise.all(usersPromises);
+
+      const users = usersResponses.map((response) => response.data);
+
+      this.posts = posts.map((post) => ({
+        ...post,
+        userName: users.find((user) => user.id === post.userId).name,
+      }));
+      this.loading = false;
+    } catch (error) {
+      console.error('Error fetching posts or users:', error);
+    }
+  },
+};
+</script>
 
 
 
